defec7319a600bb3389bec1de8cbe80e3654818e
Hari Priyan <112770032+Haripriyan3003@users.noreply.github.com>
2024-06-13 12:33:57 +0530

    php validation

--- new_task.php
+++ new_task.php
@@ -7,29 +7,37 @@
     <title>Student details</title>
 </head>
 <body>
+    <header>
+    <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item active" aria-current="page">new Student</li>
+  </ol>
+</nav>
+    </header>
+
     <div class="container">
         <h1>Student Details</h1>
         <form id="studentForm" method="post" action="submit_student.php">
-
             <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
-                <input type="text" class="form-control" name="name" id="name"  >
-                <div id="nameError" class="invalid-feedback"></div>
+                <input type="text" class="form-control <?php echo isset($errors['name']) ? 'is-invalid' : ''; ?>" name="name" id="name" value="<?php echo isset($stud_name) ? $stud_name : ''; ?>">
+                <div id="nameError" class="invalid-feedback"><?php echo isset($errors['name']) ? $errors['name'] : ''; ?></div>
             </div>
             <div class="mb-3">
                 <label for="email_id" class="form-label">Email ID</label>
-                <input type="email" class="form-control" name="email_id" id="email_id"  >
-                <div id="emailError" class="invalid-feedback"></div>
+                <input type="email" class="form-control <?php echo isset($errors['email_id']) ? 'is-invalid' : ''; ?>" name="email_id" id="email_id" value="<?php echo isset($stud_email_id) ? $stud_email_id : ''; ?>">
+                <div id="emailError" class="invalid-feedback"><?php echo isset($errors['email_id']) ? $errors['email_id'] : ''; ?></div>
             </div>
             <div class="mb-3">
                 <label for="address" class="form-label">Address</label>
-                <input type="text" class="form-control" name="address" id="address" >
-                <div id="addressError" class="invalid-feedback"></div>
+                <input type="text" class="form-control <?php echo isset($errors['address']) ? 'is-invalid' : ''; ?>" name="address" id="address" value="<?php echo isset($stud_address) ? $stud_address : ''; ?>">
+                <div id="addressError" class="invalid-feedback"><?php echo isset($errors['address']) ? $errors['address'] : ''; ?></div>
             </div>
             <div class="mb-3">
                 <label for="phone_no" class="form-label">Phone Number</label>
-                <input type="text" class="form-control" name="phone_no" id="phone_no" >
-                <div id="phoneError" class="invalid-feedback"></div>
+                <input type="text" class="form-control <?php echo isset($errors['phone_no']) ? 'is-invalid' : ''; ?>" name="phone_no" id="phone_no" value="<?php echo isset($stud_phone_no) ? $stud_phone_no : ''; ?>">
+                <div id="phoneError" class="invalid-feedback"><?php echo isset($errors['phone_no']) ? $errors['phone_no'] : ''; ?></div>
             </div>
 
             <h2>Education Details</h2>
@@ -44,134 +52,144 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                            <input type="text" class="form-control education-input" name="eduction_qualification[]" >
-                            <div class="invalid-feedback"></div>
-                        </td>
-                        <td>
-                            <input type="text" class="form-control education-input" name="instute_Name[]" >
-                            <div class="invalid-feedback"></div>
-                        </td>
-                        <td>
-                            <input type="date" class="form-control education-input" name="year_of_passing[]" >
-                            <div class="invalid-feedback"></div>
-                        </td>
-                        <td>
-                            <input type="text" class="form-control education-input" name="percentage[]" >
-                            <div class="invalid-feedback"></div>
-                        </td>
-                        <td><button type="button" class="btn btn-secondary education-input" id="addEducationRow">Add</button></td>
-                    </tr>
+                    <?php 
+                    if (!empty($education_qualification)) {
+                        foreach ($education_qualification as $index => $edu_qual) {
+                            $inst_name = isset($institute_Name[$index]) ? $institute_Name[$index] : '';
+                            $year_pass = isset($year_of_passing[$index]) ? $year_of_passing[$index] : '';
+                            $perc = isset($percentage[$index]) ? $percentage[$index] : '';
+                            ?>
+                            <tr>
+                                <td>
+                                    <input type="text" class="form-control <?php echo isset($errors["eduction_qualification_$index"]) ? 'is-invalid' : ''; ?>" name="eduction_qualification[]" value="<?php echo $edu_qual; ?>">
+                                    <div class="invalid-feedback"><?php echo isset($errors["eduction_qualification_$index"]) ? $errors["eduction_qualification_$index"] : ''; ?></div>
+                                </td>
+                                <td>
+                                    <input type="text" class="form-control <?php echo isset($errors["instute_Name_$index"]) ? 'is-invalid' : ''; ?>" name="instute_Name[]" value="<?php echo $inst_name; ?>">
+                                    <div class="invalid-feedback"><?php echo isset($errors["instute_Name_$index"]) ? $errors["instute_Name_$index"] : ''; ?></div>
+                                </td>
+                                <td>
+                                    <input type="date" class="form-control <?php echo isset($errors["year_of_passing_$index"]) ? 'is-invalid' : ''; ?>" name="year_of_passing[]" value="<?php echo $year_pass; ?>">
+                                    <div class="invalid-feedback"><?php echo isset($errors["year_of_passing_$index"]) ? $errors["year_of_passing_$index"] : ''; ?></div>
+                                </td>
+                                <td>
+                                    <input type="text" class="form-control <?php echo isset($errors["percentage_$index"]) ? 'is-invalid' : ''; ?>" name="percentage[]" value="<?php echo $perc; ?>">
+                                    <div class="invalid-feedback"><?php echo isset($errors["percentage_$index"]) ? $errors["percentage_$index"] : ''; ?></div>
+                                </td>
+                                <td><button type="button" class="btn btn-danger remove-row">Delete</button></td>
+                            </tr>
+                            <?php
+                        }
+                    } else {
+                        ?>
+                        <tr>
+                            <td>
+                                <input type="text" class="form-control" name="eduction_qualification[]">
+                                <div class="invalid-feedback"></div>
+                            </td>
+                            <td>
+                                <input type="text" class="form-control" name="instute_Name[]">
+                                <div class="invalid-feedback"></div>
+                            </td>
+                            <td>
+                                <input type="date" class="form-control" name="year_of_passing[]">
+                                <div class="invalid-feedback"></div>
+                            </td>
+                            <td>
+                                <input type="text" class="form-control" name="percentage[]">
+                                <div class="invalid-feedback"></div>
+                            </td>
+                            <td><button type="button" class="btn btn-secondary" id="addEducationRow">Add</button></td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
                 </tbody>
             </table>
             
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
-
+  
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script>
         $(document).ready(function() {
             $('#addEducationRow').click(function() {
                 var newRow = `<tr>
                     <td>
-                          <input type="text" class="form-control education-input" name="eduction_qualification[]"  >
-                          <div class="invalid-feedback"></div>
-                    </td>
-                    <td>
-                         <input type="text" class="form-control education-input" name="instute_Name[]" >
-                         <div class="invalid-feedback"></div>
-                    </td>
-                    <td>
-                        <input type="date" class="form-control education-input" name="year_of_passing[]" >
+                        <input type="text" class="form-control" name="eduction_qualification[]">
                         <div class="invalid-feedback"></div>
                     </td>
                     <td>
-                         <input type="text" class="form-control education-input" name="percentage[]"  >
-                         <div class="invalid-feedback"></div>
+                        <input type="text" class="form-control" name="instute_Name[]">
+                        <div class="invalid-feedback"></div>
+                    </td>
+                    <td>
+                        <input type="date" class="form-control" name="year_of_passing[]">
+                        <div class="invalid-feedback"></div>
+                    </td>
+                    <td>
+                        <input type="text" class="form-control" name="percentage[]">
+                        <div class="invalid-feedback"></div>
                     </td>
                     <td><button type="button" class="btn btn-danger remove-row">Delete</button></td>
                 </tr>`;
                 $('#educationTable tbody').append(newRow);
             });
-           
+            
             $(document).on('click', '.remove-row', function() {
                 $(this).closest('tr').remove();
             });
-
-            // Email validation
-            $('#email_id').on('input', function() {
-                var email = $(this).val();
-                var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-                if (!emailRegex.test(email)) {
-                    $('#emailError').text('Invalid email address');
-                    $(this).addClass('is-invalid');
-                } else {
-                    $('#emailError').text('');
-                    $(this).removeClass('is-invalid');
-                }
-            });
-
-            // Phone number validation
-            $('#phone_no').on('input', function() {
-                var phone = $(this).val();
-                var phoneRegex = /^\d{10}$/;
-                if (!phoneRegex.test(phone)) {
-                    $('#phoneError').text('Invalid phone number');
-                    $(this).addClass('is-invalid');
-                } else {
-                    $('#phoneError').text('');
-                    $(this).removeClass('is-invalid');
-                }
-            });
-
-            $('#name, #address').on('blur', function() {
-                var value = $(this).val();
-                if (value.trim() === '') {
-                    $(this).addClass('is-invalid');
-                    var fieldName = $(this).prev('label').text();
-                    $('#' + $(this).attr('id') + 'Error').text(fieldName + ' is required');
-                } else {
-                    $(this).removeClass('is-invalid');
-                    $('#' + $(this).attr('id') + 'Error').text('');
-                }
-            });
-
-            // Department, Institute Name, Year of Passing, and Percentage validation
-            $(document).on('blur', '.education-input', function() {
-                var value = $(this).val();
-                if (value.trim() === '') {
-                    $(this).addClass('is-invalid');
-                    $(this).next('.invalid-feedback').text('This field is required');
-                } else {
-                    $(this).removeClass('is-invalid');
-                    $(this).next('.invalid-feedback').text('');
-                }
-            });
-
-            $('#studentForm').on('submit', function(event) {
-                var isValid = true;
-
-                // Check all required fields
-                $('#studentForm input').each(function() {
-                    if ($(this).val().trim() === '') {
-                        $(this).addClass('is-invalid');
-                        var fieldName = $(this).prev('label').text() || $(this).closest('td').prev('th').text();
-                        $(this).next('.invalid-feedback').text(fieldName + ' is required');
-                        isValid = false;
-                    } else {
-                        $(this).removeClass('is-invalid');
-                        $(this).next('.invalid-feedback').text('');
-                    }
-                });
-
-                // Prevent form submission if validation fails
-                if (!isValid) {
-                    event.preventDefault();
-                }
-            });
         });
     </script>
+
+    <div class="container my-5">
+    <?php
+    $user = 'root';
+    $pass = '';
+    $db = 'students';
+
+    $connection = new mysqli('localhost', $user, $pass, $db) or die("Database not connected");
+
+    // Fetch student details
+    $sql_students = "SELECT * FROM students";
+    $result_students = $connection->query($sql_students);
+
+    // Fetch education details
+    $sql_education = "SELECT * FROM eductions";
+    $result_education = $connection->query($sql_education);
+    ?>
+
+    <h2>Student Management System</h2>
+     
+    <br>
+    <h3>Student Details</h3>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Student ID</th>
+                <th>Name</th>
+                <th>Email ID</th>
+                <th>Address</th>
+                <th>Phone Number</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($row = $result_students->fetch_assoc()) { ?>
+            <tr>
+                <td><?php echo $row['stud_id']; ?></td>
+                <td><?php echo $row['name']; ?></td>
+                <td><?php echo $row['email_id']; ?></td>
+                <td><?php echo $row['address']; ?></td>
+                <td><?php echo $row['phone_no']; ?></td>
+                <td>
+                    <a class='btn btn-primary btn-sm' href='edit.php?id=<?php echo $row['stud_id']; ?>'>Update</a>
+                    <a class='btn btn-danger btn-sm' href='delete.php?id=<?php echo $row['stud_id']; ?>'>Delete</a>
+                </td>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 </body>
 </html>
--- submit_student.php
+++ submit_student.php
@@ -6,6 +6,8 @@ $db = 'students';
 // Connect to the database
 $connection = new mysqli('localhost', $user, $pass, $db) or die("Database not connected");
 
+$errors = [];
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get student details from the form
     $stud_name = $_POST['name'];
@@ -13,19 +15,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stud_address = $_POST['address'];
     $stud_phone_no = $_POST['phone_no'];
 
-    // Insert into students table (assuming Stud_id is auto-increment)
-    $sql = "INSERT INTO students (name, email_id, address, phone_no) 
-             VALUES ('$stud_name', '$stud_email_id', '$stud_address', '$stud_phone_no')";
+    // Validate student details
+    if (empty($stud_name)) {
+        $errors['name'] = 'Name is required';
+    }
+    if (empty($stud_email_id) || !filter_var($stud_email_id, FILTER_VALIDATE_EMAIL)) {
+        $errors['email_id'] = 'Valid email ID is required';
+    }
+    if (empty($stud_address)) {
+        $errors['address'] = 'Address is required';
+    }
+    if (empty($stud_phone_no) || !preg_match('/^\d{10}$/', $stud_phone_no)) {
+        $errors['phone_no'] = 'Valid phone number is required';
+    }
+
+    // Get education details from the form
+    $education_qualification = $_POST['eduction_qualification'];
+    $institute_Name = $_POST['instute_Name'];
+    $year_of_passing = $_POST['year_of_passing'];
+    $percentage = $_POST['percentage'];
+
+    // Validate education details
+    foreach ($education_qualification as $index => $value) {
+        if (empty($value)) {
+            $errors["eduction_qualification_$index"] = 'Education qualification is required';
+        }
+        if (empty($institute_Name[$index])) {
+            $errors["instute_Name_$index"] = 'Institute Name is required';
+        }
+        if (empty($year_of_passing[$index])) {
+            $errors["year_of_passing_$index"] = 'Year of Passing is required';
+        }
+        if (empty($percentage[$index])) {
+            $errors["percentage_$index"] = 'Percentage is required';
+        }
+    }
+
+    // If there are errors, output them and stop the script
+    if (!empty($errors)) {
+        include 'new_task.php';
+        exit;
+    }
+
+    // Insert into students table
+    $sql = "INSERT INTO students (name, email_id, address, phone_no) VALUES ('$stud_name', '$stud_email_id', '$stud_address', '$stud_phone_no')";
     $result = $connection->query($sql);
 
     if ($result) {
         $student_id = $connection->insert_id; // Get the ID of the last inserted record
-
-        // Get education details from the form
-        $education_qualification = $_POST['eduction_qualification'];
-        $institute_Name = $_POST['instute_Name'];
-        $year_of_passing = $_POST['year_of_passing'];
-        $percentage = $_POST['percentage'];
 
         // Loop through the education details and insert them into the eductions table
         for ($i = 0; $i < count($education_qualification); $i++) {
@@ -39,18 +76,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $result = $connection->query($sql);
 
             if (!$result) {
-                echo "Error: " . $sql . "<br>" . $connection->error;
+                $errors['database'] = $connection->error;
+                include 'new_task.php';
                 exit;
             }
         }
-          echo '<script> alert("Data inserted successfully!!!");  </script>'; 
-         echo '<script>window.location.href ="new_task.php" ;</script>';
+
         // Redirect after successful submission
-       // header("Location: new_task.php");
+        echo '<script>alert("Data inserted successfully!!!");</script>'; 
+        echo '<script>window.location.href="new_task.php";</script>';
         exit;
     } else {
-        echo "Error: " . $sql . "<br>" . $connection->error;
+        $errors['database'] = $connection->error;
+        include 'new_task.php';
     }
+} else {
+    $errors['request'] = 'Invalid request method';
+    include 'new_task.php';
 }
 ?>
-
